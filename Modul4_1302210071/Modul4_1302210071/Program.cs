c860c25c8ac2fa3fad92352c2b3453ecf276f3d4
Your Name <afnylatifa@gmail.com>
2023-03-14 16:49:14 +0700

    Menambahkan kode baru

--- Modul4_1302210071/Modul4_1302210071/Program.cs
+++ Modul4_1302210071/Modul4_1302210071/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class kodeBuah
+{
+    public enum NamaBuah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blacberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka };
+
+    public static String GetKodeBuah(NamaBuah Buah)
+    {
+        String[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00", };
+        return kodeBuah[(int)Buah];
+    }
+
+    public static void main()
+    {
+        Console.WriteLine("Nama Buah" + kodeBuah.NamaBuah.Semangka + "Kode Buah" + kodeBuah.GetKodeBuah(kodeBuah.NamaBuah.Semangka));
+    }
+}
+
+
